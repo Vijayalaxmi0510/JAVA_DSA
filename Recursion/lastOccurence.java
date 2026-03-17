096e4f80ee810819cd3ed73d5a302d89aa41e6ef
Vijayalaxmi Biradar <vijayalaxmibiradar793@gmail.com>
2026-03-18 02:21:52 +0530

    Add files via upload

---
+++ Recursion/lastOccurence.java
@@ -0,0 +1,19 @@
+public class lastOccurence {
+    public static int lastOccurence(int arr[],int key,int i){
+       if(i==arr.length){
+           return -1;
+       }
+        int isFound=lastOccurence( arr, key, i+1);
+        if(isFound != -1){
+            return isFound;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+        return isFound;
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,4,5,2,3,4,5,6};
+        System.out.println(lastOccurence(arr,5,0));
+    }
+}
